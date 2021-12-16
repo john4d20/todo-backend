4b2cb9070d97f82e9c14514e96f12b25e7e9a0a3
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-16 19:28:17 +0800

    [todoList backend][john] feat:pass should_return_updated_company_when_update_company_given_company_company_id

--- src/test/java/com/example/todo/integration/TodoIntegrationTest.java
+++ src/test/java/com/example/todo/integration/TodoIntegrationTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -46,6 +49,40 @@ public class TodoIntegrationTest {
                 .andExpect(jsonPath("$[0].id").isString())
                 .andExpect(jsonPath("$[0].content").value("test"))
                 .andExpect(jsonPath("$[0].done").value(true));
+    }
+
+//    @Test
+//    public void should_return_todo_when_create_todo_given_todo() throws Exception {
+//
+//        String todoAsJson = "{\n" +
+//                "    \"content\" : \"test\",\n" +
+//                "    \"done\": true\n" +
+//                "}";
+//        mockMvc.perform(MockMvcRequestBuilders.post("/todos")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(todoAsJson))
+//                .andExpect(status().isCreated())
+//                .andExpect(jsonPath("$[0].id").isString())
+//                .andExpect(jsonPath("$[0].content").value("test"))
+//                .andExpect(jsonPath("$[0].done").value(true));
+//    }
+
+    @Test
+    public void should_return_updated_company_when_update_company_given_company_company_id() throws Exception {
+        //given
+        Todo todo = todoRepository.save(new Todo("test", true));
+        String todoAsJson = "{\n" +
+                "    \"content\" : \"test\",\n" +
+                "    \"done\": true\n" +
+                "}";
+        System.out.println("odo.getId()"+todo.getId());
+        mockMvc.perform(MockMvcRequestBuilders.put("/todos/" + todo.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(todoAsJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isString())
+                .andExpect(jsonPath("$.content").value("test"))
+                .andExpect(jsonPath("$.done").value(true));
     }
 
 }
