a5cfca7b55c214c965be74c5ff6a7db2a9a33600
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-16 18:12:16 +0800

    [todoList backend][john] feat:add cors

--- src/main/java/com/example/todo/contoller/TodoController.java
+++ src/main/java/com/example/todo/contoller/TodoController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/todos")
 public class TodoController {
     @Autowired
